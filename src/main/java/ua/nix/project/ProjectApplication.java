48f0802f2cc3bff9cb98e025ebe1226399e27c49
Roman <ivaniv.roma.15@gmail.com>
2023-01-29 22:25:12 +0200

    creating StudentController methods and templates for adding, modifiying, deleting students

--- src/main/java/ua/nix/project/ProjectApplication.java
+++ src/main/java/ua/nix/project/ProjectApplication.java
@@ -20,20 +20,20 @@ public class ProjectApplication {
   }
 
 
-  @Bean
-  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-    return args -> {
-
-      System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-      String[] beanNames = ctx.getBeanDefinitionNames();
-      Arrays.sort(beanNames);
-      for (String beanName : beanNames) {
-        System.out.println(beanName);
-      }
-
-      studentService.createStudent("Mark", "mark1234@gmail.com");
-    };
-  }
+//  @Bean
+//  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//    return args -> {
+//
+//      System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//      String[] beanNames = ctx.getBeanDefinitionNames();
+//      Arrays.sort(beanNames);
+//      for (String beanName : beanNames) {
+//        System.out.println(beanName);
+//      }
+//
+//      studentService.createStudent("Mark", "mark1234@gmail.com");
+//    };
+//  }
 
 }
--- src/main/java/ua/nix/project/controller/StudentController.java
+++ src/main/java/ua/nix/project/controller/StudentController.java
@@ -1,10 +1,82 @@
 package ua.nix.project.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ua.nix.project.repository.StudentRepository;
+import ua.nix.project.repository.entity.StudentEntity;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
 public class StudentController {
 
+    @Autowired
+    private StudentRepository studentRepository;
 
+    @GetMapping("/students")
+    public String homePage(Model model){
+        Iterable<StudentEntity> students = studentRepository.findAll();
+        model.addAttribute("students", students);
+        return "students";
+    }
+
+    @GetMapping("/students/{id}")
+    public String studentDetails(@PathVariable(value = "id") long id, Model model) {
+        if(!studentRepository.existsById(id))
+            return "redirect:/students";
+
+        Optional<StudentEntity> student = studentRepository.findById(id);
+        List<StudentEntity> res = new ArrayList<>();
+        student.ifPresent(res::add);
+        model.addAttribute("student", res);
+        return "student-detail";
+    }
+
+    @GetMapping("/students/add")
+    public String addStudent(Model model) {
+        return "student-add";
+    }
+
+    @PostMapping("/students/add")
+    public String addStudentPost(@RequestParam String name, @RequestParam String email, Model model) {
+        StudentEntity student = new StudentEntity();
+        student.setName(name);
+        student.setEmail(email);
+        studentRepository.save(student);
+        return "redirect:/students";
+    }
+
+    @PostMapping("/students/{id}")
+    public String studentDelete(@PathVariable(value = "id") long id, Model model) {
+        StudentEntity student = studentRepository.findById(id).orElseThrow();
+        studentRepository.delete(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/{id}/edit")
+    public String studentEdit(@PathVariable(value = "id") long id,  Model model) {
+        if(!studentRepository.existsById(id))
+            return "redirect:/students";
+
+        Optional<StudentEntity> student = studentRepository.findById(id);
+        List<StudentEntity> res = new ArrayList<>();
+        student.ifPresent(res::add);
+        model.addAttribute("student", res);
+        return "student-edit";
+    }
+
+    @PostMapping("/students/{id}/edit")
+    public String studentUpdate(@PathVariable(value = "id") long id, @RequestParam String name, @RequestParam String email,
+                               Model model) {
+        StudentEntity student = studentRepository.findById(id).orElseThrow();
+        student.setName(name);
+        student.setEmail(email);
+        studentRepository.save(student);
+        return "redirect:/students";
+    }
 }
